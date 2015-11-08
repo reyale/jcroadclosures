0a360b8c1870b1695cdd0c3e163549cd61d485ec
alex <alex@alex-VirtualBox.(none)>
2015-11-08 18:19:07 -0500

    adding page for data entry

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ class MainHandler(tornado.web.RequestHandler):
         self.render("index.html")
 
 class DBHandler(tornado.web.RequestHandler):
-    def get(self, date):
+    def get(self, date=None):
         date = self.get_argument("DATE", None) 
         if not date:
           self.write(json.dumps([]))
@@ -19,9 +19,14 @@ class DBHandler(tornado.web.RequestHandler):
 
         self.write(Database.db_to_json(database, date))
 
+class EntryHandler(tornado.web.RequestHandler):
+    def get(self, data=None):
+        self.render("entry.html")
+
 def make_app():
     return tornado.web.Application([
         (r"/get_data(.*)", DBHandler),
+        (r"/entry(.*)", EntryHandler),
         (r"/", MainHandler)
     ])
 
